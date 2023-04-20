157cb1160f8a18bd0757c9b3f4c3c1e9b36082d0
ayoubBDL <devfox66@gmail.com>
2023-04-20 09:58:05 +0000

    first commit

---
+++ client/src/app/products/products.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from '../models/models';
+import { NavigationService } from '../services/navigation.service';
+import { UtilityService } from '../services/utility.service';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css'],
+})
+export class ProductsComponent implements OnInit {
+  view: 'grid' | 'list' = 'list';
+  sortby: 'default' | 'htl' | 'lth' = 'default';
+  products: Product[] = [];
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private navigationService: NavigationService,
+    private utilityService: UtilityService
+  ) {}
+
+  ngOnInit(): void {
+    this.activatedRoute.queryParams.subscribe((params: any) => {
+      let category = params.category;
+      let subcategory = params.subcategory;
+
+      if (category && subcategory)
+        this.navigationService
+          .getProducts(category, subcategory, 10)
+          .subscribe((res: any) => {
+            this.products = res;
+          });
+    });
+  }
+
+  sortByPrice(sortKey: string) {
+    this.products.sort((a, b) => {
+      if (sortKey === 'default') {
+        return a.id > b.id ? 1 : -1;
+      }
+      return (
+        (sortKey === 'htl' ? 1 : -1) *
+        (this.utilityService.applyDiscount(a.price/100, a.offer.discount/100) >
+        this.utilityService.applyDiscount(b.price/100, b.offer.discount/100)
+          ? -1
+          : 1)
+      );
+    });
+  }
+}
---
+++ client/src/app/services/utility.service.ts
@@ -0,0 +1,103 @@
+import { Injectable } from '@angular/core';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { Subject, window } from 'rxjs';
+import { Cart, Payment, Product, User } from '../models/models';
+import { NavigationService } from './navigation.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UtilityService {
+  changeCart = new Subject();
+
+  constructor(
+    private navigationService: NavigationService,
+    private jwt: JwtHelperService
+  ) {}
+
+  applyDiscount(price: number, discount: number): number {
+    let finalPrice: number = (price - price * (discount / 100));
+    return finalPrice;
+  }
+
+  // JWT Helper Service : npm install @auth0/angular-jwt
+
+  getUser(): User {
+    let token = this.jwt.decodeToken();
+    let user: User = {
+      id: token.id,
+      firstName: token.firstName,
+      lastName: token.lastName,
+      address: token.address,
+      mobile: token.mobile,
+      email: token.email,
+      password: '',
+      createdAt: token.createdAt,
+      modifiedAt: token.modifiedAt,
+    };
+    return user;
+  }
+
+  setUser(token: string) {
+    localStorage.setItem('user', token);
+  }
+
+  isLoggedIn() {
+    return localStorage.getItem('user') ? true : false;
+  }
+
+  logoutUser() {
+    localStorage.removeItem('user');
+  }
+
+  addToCart(product: Product) {
+    let productid = product.id;
+    let userid = this.getUser().id;
+
+    this.navigationService.addToCart(userid, productid).subscribe((res) => {
+      if (res.toString() === 'inserted') this.changeCart.next(1);
+    });
+  }
+
+  calculatePayment(cart: Cart, payment: Payment) {
+    payment.totalAmount = 0;
+    payment.amountPaid = 0;
+    payment.amountReduced = 0;
+
+    for (let cartitem of cart.cartItems) {
+      payment.totalAmount += cartitem.product.price;
+
+      payment.amountReduced +=
+        cartitem.product.price -
+        this.applyDiscount(
+          cartitem.product.price,
+          cartitem.product.offer.discount
+        );
+
+      payment.amountPaid += this.applyDiscount(
+        cartitem.product.price,
+        cartitem.product.offer.discount
+      );
+    }
+
+    if (payment.amountPaid > 500) payment.shipingCharges = 20;
+    else if (payment.amountPaid > 200) payment.shipingCharges = 10;
+    else if (payment.amountPaid > 50) payment.shipingCharges = 5;
+    else payment.shipingCharges = 2;
+  }
+
+  calculatePricePaid(cart: Cart) {
+    let pricepaid = 0;
+    for (let cartitem of cart.cartItems) {
+      pricepaid += this.applyDiscount(
+        cartitem.product.price/100,
+        cartitem.product.offer.discount/100
+      );
+    }
+    return pricepaid;
+  }
+
+  orderTheCart() {
+    
+  }
+}
